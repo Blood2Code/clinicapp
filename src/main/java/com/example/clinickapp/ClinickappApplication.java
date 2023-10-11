6946ce8eefb9c0fd6b409d9689444601c2822969
umar <abduxalilraxmonberdiyev@gmail.com>
2023-10-11 10:46:58 +0500

    start

---
+++ src/main/java/com/example/clinickapp/ClinickappApplication.java
@@ -0,0 +1,13 @@
+package com.example.clinickapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinickappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinickappApplication.class, args);
+    }
+
+}
