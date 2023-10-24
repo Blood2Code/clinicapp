93f98570ea56c648c1d61c1c0975acee0fd37207
umar <abduxalilraxmonberdiyev@gmail.com>
2023-10-24 12:21:48 +0500

    [+] patient dto created successfully and working

---
+++ src/main/java/com/example/clinickapp/dto/PatientDto.java
@@ -0,0 +1,32 @@
+package com.example.clinickapp.dto;
+
+import com.example.clinickapp.entity.Treatments;
+import com.example.clinickapp.entity.Utilization;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PatientDto {
+    private Long id;
+    private String name;
+    private String surname;
+    private String gender;
+    private Date birthDate;
+    private String passportSerial;
+    private Date receivedDate;
+    private Integer wardNumber;
+    private Integer wardBedNumber;
+    private Date wardedDate;
+    private String illness;
+    private String diagnosis;
+    private List<TreatmentsDto> treatments;
+    private List<UtilizationDto> utilizationList;
+}
