7d5ecbc55ba34de6a9a32d2b6d92b250ef3b0a43
umar <abduxalilraxmonberdiyev@gmail.com>
2023-10-29 21:58:32 +0500

    [*] PatientController changed to mvc controller

--- src/main/java/com/example/clinickapp/controller/PatientController.java
+++ src/main/java/com/example/clinickapp/controller/PatientController.java
@@ -5,33 +5,46 @@ import com.example.clinickapp.dto.ResponseDto;
 import com.example.clinickapp.service.PatientService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("api/patients")
 @RequiredArgsConstructor
 @Api(tags = "Patient Management")
 public class PatientController {
     private final PatientService patientService;
+
+    @GetMapping
+    @ApiOperation(value = "View patient page")
+    public String patientPage(Model model) {
+        return "/patient";
+    }
+
     @PostMapping("/create")
     @ApiOperation(value = "Create patient")
-    public ResponseEntity<ResponseDto<PatientDto>> createPatient(@RequestBody PatientDto patientDto) {
+    public String createPatient(@ModelAttribute PatientDto patientDto) {
         ResponseDto<PatientDto> response = patientService.createPatient(patientDto);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        return "/patient";
     }
     @GetMapping("/all")
     @ApiOperation(value = "Get patients")
-    public ResponseEntity<ResponseDto<List<PatientDto>>> getAllPatients() {
-        return ResponseEntity.status(HttpStatus.OK).body(patientService.getAllPatients());
+    public String getAllPatients(Model model) {
+        model.addAttribute("patientList", patientService.getAllPatients());
+        return "/patients";
     }
     @GetMapping("/{id}")
     @ApiOperation(value = "Get patient")
-    public ResponseEntity<ResponseDto<PatientDto>> getPatientById(@PathVariable Long id) {
+    public ResponseEntity<ResponseDto<PatientDto>> getPatientById(@PathVariable Long id, Model model) {
+        model.addAttribute("asd", patientService.getPatientById(id));
         return ResponseEntity.status(HttpStatus.OK).body(patientService.getPatientById(id));
     }
     @PutMapping("/update")
