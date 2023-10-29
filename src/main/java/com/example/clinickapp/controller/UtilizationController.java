e95d8aa68d6eecf1fad5c839028db9283869eeea
umar <abduxalilraxmonberdiyev@gmail.com>
2023-10-29 22:02:24 +0500

    [+] Utilization controller changed to mvc controller

--- src/main/java/com/example/clinickapp/controller/UtilizationController.java
+++ src/main/java/com/example/clinickapp/controller/UtilizationController.java
@@ -7,11 +7,12 @@ import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("api/utilization")
 @RequiredArgsConstructor
 @Api(tags = "Utilization Management")
