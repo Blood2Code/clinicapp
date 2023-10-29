2535675aed4679fb2522a6f8d440664a9a48035f
umar <abduxalilraxmonberdiyev@gmail.com>
2023-10-29 22:01:02 +0500

    [+] UserLogin added password

--- src/main/java/com/example/clinickapp/entity/UserLogin.java
+++ src/main/java/com/example/clinickapp/entity/UserLogin.java
@@ -7,16 +7,17 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserLogin {
+public class UserLogin{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String surname;
+    private String password;
     private String role;
+
 }
